1fb3251e9a5d3205c4dfeb3bbe4b0b163c055a0f
bulat-gab <gabdrakhmanov.br@gmail.com>
2022-04-12 14:46:43 +0700

    Move exceptions to a common project

---
+++ src/EduPlan.ChatApp.Common/Exceptions/ChatAppUserDoesNotExistException.cs
@@ -0,0 +1,8 @@
+﻿namespace EduPlan.ChatApp.Common.Exceptions;
+
+public class ChatAppUserDoesNotExistException : ChatAppException
+{
+    public ChatAppUserDoesNotExistException(string message) : base(message)
+    {
+    }
+}
